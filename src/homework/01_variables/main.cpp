39d63000a8428258a8f35c99cd1a3a4d3acd887e
ShapesGH <shapes117@gmail.com>
2022-01-29 19:41:52 -0600

    HW 01 with test cases

--- src/homework/01_variables/main.cpp
+++ src/homework/01_variables/main.cpp
@@ -1,13 +1,25 @@
 ﻿//write include statements
+#include "variables.h"
+#include<iostream>
 
 
 //write namespace using statement for cout
-
-
+using std::cout; using std::cin;
 
 int main()
 {
+	int num;
+	int result;
+	int num1 = 4;
 	
+	cout<<"Please type in an integer:"<<'\n';
+	cin >> num;
+
+	result = multiply_numbers(num);
+	cout<<"The first result is: " << result << '\n';
+		
+	result = multiply_numbers(num1);
+	cout<<"The second result is: "<<result<< '\n';
 
 	return 0;
 }
