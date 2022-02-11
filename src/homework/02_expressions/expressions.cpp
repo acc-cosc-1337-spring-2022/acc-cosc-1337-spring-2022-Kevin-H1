98f66b9d5cd8a9f06c53bd1cb78fbfd5951f0140
ShapesGH <shapes117@gmail.com>
2022-02-11 13:38:43 -0600

    HW 02 with test cases

--- src/homework/02_expressions/expressions.cpp
+++ src/homework/02_expressions/expressions.cpp
@@ -1,5 +1,7 @@
 #include "expressions.h"
 
+
+
 //example
 int add_numbers(int num1, int num2)
 {
@@ -9,6 +11,12 @@ int add_numbers(int num1, int num2)
 //write code for multiply_numbers function here
 //function returns product of num1 and num2
 
+double get_sales_tax_amount(double meal_amount)
+{
+	return TAX_RATE * meal_amount;
+}
 
-
-
+double get_tip_amount(double meal_amount, double tip_rate)
+{
+	return meal_amount * (tip_rate/100);
+}
