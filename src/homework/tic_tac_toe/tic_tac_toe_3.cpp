fcb4867590290caf64171803922cc22bf299a134
ShapesGH <shapes117@gmail.com>
2022-04-16 20:46:28 -0500

    hw-09 w/Test Cases

--- src/homework/tic_tac_toe/tic_tac_toe_3.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe_3.cpp
@@ -1,5 +1,7 @@
 #include "tic_tac_toe_3.h"
 
+
+
 /*
 class function check_column_win
 Win by column if and return true if
@@ -9,6 +11,37 @@ Win by column if and return true if
 else
 false
 */
+
+bool TicTacToe3::check_column_win()
+{
+    if(pegs[0] == "X" && pegs[3] == "X" && pegs[6] == "X")
+    {
+        return true;
+    }
+    else if(pegs[0] == "O" && pegs[3] == "O" && pegs[6] == "O")
+    {
+        return true;
+    }
+    else if(pegs[1] == "X" && pegs[4] == "X" && pegs[7] == "X")
+    {
+        return true;
+    }
+    else if(pegs[1] == "O" && pegs[4] == "O" && pegs[7] == "O")
+    {
+        return true;
+    }
+    else if(pegs[2] == "X" && pegs[5] == "X" && pegs[8] == "X")
+    {
+        return true;
+    }
+    else if(pegs[2] == "O" && pegs[5] == "O" && pegs[8] == "O")
+    {
+        return true;
+    }
+    return false;
+
+}
+
 
 
 
@@ -20,6 +53,39 @@ Win by row if
 6,7,8 are equal
 */
 
+bool TicTacToe3::check_row_win()
+{
+    //first row
+    if(pegs[0] == "X" && pegs[1] == "X" && pegs[2] == "X")
+    {
+        return true;
+    }
+    else if(pegs[0] == "O" && pegs[1] == "O" && pegs[2] == "O")
+    {
+        return true;
+    }   
+    //second row
+    else if(pegs[3] == "X" && pegs[4] == "X" && pegs[5] == "X")
+    {
+        return true;
+    }
+    else if(pegs[3] == "O" && pegs[4] == "O" && pegs[5] == "O")
+    {
+        return true;
+    }
+    //third row 
+    else if(pegs[6] == "X" && pegs[7] == "X" && pegs[8] == "X")
+    {
+        return true;
+    }
+    else if(pegs[6] == "X" && pegs[7] == "X" && pegs[8] == "X")
+    {
+        return true;
+    }
+    return false;
+
+}
+
 
 
 /*
@@ -30,3 +96,24 @@ Win diagonally
 6 7 8
 
 */
+
+bool TicTacToe3::check_diagonal_win()
+{
+    if(pegs[0] == "X" && pegs[4] == "X" && pegs[8] == "X")
+    {
+        return true;
+    }
+    else if(pegs[2] == "X" && pegs[4] == "X" && pegs[6] == "X") 
+    {
+        return true;
+    }
+    else if (pegs[0] == "O" && pegs[4] == "O" && pegs[8] == "O")
+    {
+        return true;
+    }
+    else if (pegs[2] == "O" && pegs[4] == "O" && pegs[6] == "O")
+    {
+        return true;
+    }
+    return false;
+}
--- src/homework/tic_tac_toe/tic_tac_toe_4.h
+++ src/homework/tic_tac_toe/tic_tac_toe_4.h
@@ -1,1 +1,23 @@
 //h
+#include "tic_tac_toe.h"
+
+#ifndef TIC_TAC_TOE_4_H
+#define TIC_TAC_TOE_4_H
+
+class TicTacToe4: public TicTacToe
+{
+
+public:
+    //Constructor
+    TicTacToe4(): TicTacToe(4){}
+
+private:
+    bool check_column_win()override;
+    bool check_row_win()override;
+    bool check_diagonal_win()override;
+
+};
+
+
+
+#endif
